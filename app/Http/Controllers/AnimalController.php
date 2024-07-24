a285e3e66ec22e602e4f92a42842484cacd03db8
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-24 16:20:08 +0200

    added some comments in the controller

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -14,7 +14,7 @@ class AnimalController extends Controller
     public function index()
     {
         $animals = Animal::all();
-        return view('animals.index', compact("animals"));
+        return view('animals.index', compact("animals")); //mostra tutti gli elementi
     }
 
     /**
@@ -22,7 +22,7 @@ class AnimalController extends Controller
      */
     public function create()
     {
-        return view('animals.create');
+        return view('animals.create'); //rimanda al form per creare un nuovo elemento
     }
 
     /**
@@ -30,18 +30,19 @@ class AnimalController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->all();
-        $newAnimal = new Animal($data); // Mass assignment
+        $data = $request->all(); //Richiedo i dati
+        $newAnimal = new Animal($data); // Mass assignment tramite le fillable già inserite nel model Animal
         // $newAnimal->name = $data["name"];
         // $newAnimal->species = $data["species"];
         // $newAnimal->breed = $data["breed"];  Mano
         // $newAnimal->image_url = $data["image_url"];
         // $newAnimal->weight = $data["weight"];
         // $newAnimal->description = $data["description"];
-        $newAnimal->save();
+        $newAnimal->save(); // Salvo
+        // ! $newAnimal = Animal::create($data); equivale a new Animal + save
         // > dd($newAnimal); Testiamo se è tutto corretto
         // ? dd($data);
-        return redirect()->route('animals.show', $newAnimal);
+        return redirect()->route('animals.show', $newAnimal); // Crea l'elemento con i dati passati dal form e ci mostra l'elemento in show
     }
 
     /**
@@ -49,7 +50,7 @@ class AnimalController extends Controller
      */
     public function show(Animal $animal)
     {
-        return view('animals.show', compact("animal"));
+        return view('animals.show', compact("animal")); // mostro il singolo elemento
     }
 
     /**
@@ -57,7 +58,7 @@ class AnimalController extends Controller
      */
     public function edit(Animal $animal)
     {
-        return view('animals.edit', compact('animal'));
+        return view('animals.edit', compact('animal')); //ritorno il form
     }
 
     /**
@@ -66,10 +67,10 @@ class AnimalController extends Controller
     public function update(Request $request, Animal $animal)
     {
         // dd($request->all());
-        $data = $request->all();
-        $animal->update($data);
+        $data = $request->all(); //richiedo tutti i dati
+        $animal->update($data); // modifico i dati del singolo animale attraverso il mio form con i value già presenti
         return redirect()->route('animals.show', $animal)->with('message', $animal->name . " Has Been Edited");
-    }
+    }                                                     //with ci permette di inviare il value di message così possiamo visualizzarlo dopo avere cliccato UPDATE
 
     /**
      * Remove the specified resource from storage.
@@ -78,5 +79,5 @@ class AnimalController extends Controller
     {
         $animal->delete();
         return redirect()->route('animals.index')->with('message', "N:" . $animal->id . " " . $animal->name . " Has Been Deleted");
-    }
+    }                                               //with ci permette di inviare il value di message così possiamo visualizzarlo dopo avere cliccato DELETE
 }
